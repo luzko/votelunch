9c11a94b1ab015870cee5809c5c64e0634408e3a
Dmitry Luzko <dmitrii.luzko@gmail.com>
2020-07-06 15:20:48 +0300

    add spring

---
+++ src/main/java/by/luzko/votelunch/util/ValidationUtil.java
@@ -0,0 +1,41 @@
+package by.luzko.votelunch.util;
+
+import by.luzko.votelunch.model.AbstractBaseEntity;
+import by.luzko.votelunch.util.exception.NotFoundException;
+
+public class ValidationUtil {
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractBaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
+        }
+    }
+}
---
+++ src/main/java/by/luzko/votelunch/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package by.luzko.votelunch.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
